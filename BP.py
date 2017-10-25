64f9812724911c804dd302daa23988079e6f09dc
wanglijun <juns1984@qq.com>
2017-10-25 16:58:51 +0800

    update

--- BP.py
+++ BP.py
@@ -10,7 +10,8 @@ def SGD():
     x[:] = x - 0.001 * x.grad
 
 
-def test():
+# min |x + 7|
+def min_x7():
     a = nd.array((1,))
     with autograd.record():
         b = nd.dot(a, x) + 7
@@ -21,5 +22,5 @@ def test():
 
 if __name__ == '__main__':
     for i in range(10000):
-        test()
+        min_x7()
     print(x)
