a1f622aaa62d66b04f094bbc6f489601edc28772
wanglijun <juns1984@qq.com>
2017-10-25 17:44:23 +0800

    update

--- BP.py
+++ BP.py
@@ -1,6 +1,7 @@
 from mxnet import autograd
 from mxnet import ndarray as nd
 
+a = nd.array((1,))
 x = nd.array((-40,))
 x.attach_grad()
 
@@ -9,18 +10,20 @@ def SGD():
     x[:] = x - 0.001 * x.grad
 
 
-# min |x + 7|
+def net(a, b):
+    return nd.dot(a, nd.abs(b)) - 7
+
+
+# min ||x| + 7|
 def min_x7():
-    a = nd.array((1,))
     with autograd.record():
-        b = nd.dot(a, x) + 7
+        b = net(a, x)
         diff = b ** 2
     diff.backward()
-    print(x.grad)
     SGD()
 
 
 if __name__ == '__main__':
     for i in range(10000):
         min_x7()
-    print(x)
+    print(net(a,x))
