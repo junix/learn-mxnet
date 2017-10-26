4c9f6bb68f7f3620659dcdb9eb826300132be1bc
wanglijun <juns1984@qq.com>
2017-10-26 09:31:45 +0800

    update

---
+++ grad3.py
@@ -0,0 +1,36 @@
+from mxnet import autograd
+from mxnet import ndarray as nd
+
+a = nd.array(((1, 2), (3, 4)))
+# a = nd.array((1, 2))
+# a.attach_grad()
+
+x = nd.array((1, 1))
+x.attach_grad()
+
+
+def SGD():
+    x[:] = x - 0.001 * x.grad
+
+
+def f(x):
+    """
+    f([x1,x2]) = |x1|**2 + |x2|**3
+    ▽f = [2*x1, 3*x2**2]
+    """
+    z = nd.dot(a, x)
+    return nd.dot(z.T, z)
+
+
+def cal_grad():
+    with autograd.record():
+        y = f(x)
+    y.backward()
+    print(x.grad)
+    # SGD()
+
+
+if __name__ == '__main__':
+    # for i in range(10000):
+    cal_grad()
+    # print(f(x))  # 5
