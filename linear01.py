89c5fe49db14812ef1c2c55fba4529fb24a5aac8
wanglijun <juns1984@qq.com>
2017-10-26 15:09:25 +0800

    update

---
+++ linear01.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+# ======================== Basic autograd example 2 =======================#
+# Create tensors.
+x = Variable(torch.randn(5, 3))
+y = Variable(torch.randn(5, 2))
+
+# Build a linear layer.
+linear = nn.Linear(3, 2)
+print('w: ', linear.weight)
+print('b: ', linear.bias)
+
+# Build Loss and Optimizer.
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(linear.parameters(), lr=0.01)
+
+# Forward propagation.
+pred = linear(x)
+
+# Compute loss.
+loss = criterion(pred, y)
+print('loss: ', loss.data[0])
+
+# Backpropagation.
+loss.backward()
+
+# Print out the gradients.
+print('dL/dw: ', linear.weight.grad)
+print('dL/db: ', linear.bias.grad)
+
+# 1-step Optimization (gradient descent).
+optimizer.step()
+
+# You can also do optimization at the low level as shown below.
+# linear.weight.data.sub_(0.01 * linear.weight.grad.data)
+# linear.bias.data.sub_(0.01 * linear.bias.grad.data)
+
+# Print out the loss after optimization.
+pred = linear(x)
+loss = criterion(pred, y)
+print('loss after 1 step optimization: ', loss.data[0])
